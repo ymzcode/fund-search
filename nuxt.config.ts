d11c3c2f8c2b279bbfc7b17152cce6a47459cd29
吃饱了不饿 <ymzcode@163.com>
2024-08-16 09:54:04 +0800

    打包

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "node:path";
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: ['@pinia/nuxt', "@ant-design-vue/nuxt"],
     antd: {
         extractStyle: false
+    },
+    experimental: {
+        payloadExtraction: false
+    },
+    app: {
+        baseURL: '/fund-search/',
+        buildAssetsDir: 'nuxt_assets',
+    },
+    nitro: {
+        output: {
+            publicDir: path.join(__dirname, 'docs'),
+        }
     }
 })
